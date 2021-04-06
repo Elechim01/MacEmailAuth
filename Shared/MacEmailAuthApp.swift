4be659938043a792b5318509aeea26dfbf08fa9a
Elechim01 <mikymanny015@gmail.com>
2021-04-06 21:59:39 +0200

    Initial Commit

---
+++ Shared/MacEmailAuthApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacEmailAuthApp.swift
+//  Shared
+//
+//  Created by Michele Manniello on 06/04/21.
+//
+
+import SwiftUI
+
+@main
+struct MacEmailAuthApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
